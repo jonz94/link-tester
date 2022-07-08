3721c313b5107adb1581963e64bb2b559c38ac94
jonz94 <jody16888@gmail.com>
2022-07-08 17:25:42 +0800

    fix: RWD

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,15 @@ onMounted(() => input.value?.focus())
     <h1 class="my-8 text-2xl font-black">Link Tester</h1>
     <div>
       <input
-        class="block w-full rounded-md border-transparent bg-gray-100 focus:border-gray-500 focus:bg-white focus:ring-0"
+        class="block w-full rounded-md border-transparent bg-gray-100 focus:border-gray-500 focus:bg-white focus:ring-0 sm:min-w-[28rem]"
         type="text"
-        placeholder="Input yout link"
+        placeholder="Input yout link, example: https://link-tester.jonz94.dev"
         v-model="link"
         ref="input"
       />
-      <p class="mt-4 text-center">
-        <a class="text-lg underline" :href="link" target="_blank">open {{ link }}</a>
+      <p class="mt-4 max-w-[100vw] space-x-2 break-all text-center text-lg">
+        <span>open</span>
+        <a class="underline" :href="link" target="_blank">{{ link }}</a>
       </p>
     </div>
   </div>
